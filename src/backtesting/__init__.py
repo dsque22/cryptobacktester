e3d68861ab2249cba60992bf7e5f0891b1fb3368
dsque22 <pietrorcc@yahoo.com>
2025-06-06 11:06:28 -0700

    Update __init__.py

--- src/backtesting/__init__.py
+++ src/backtesting/__init__.py
@@ -1,5 +1,5 @@
 """Backtesting engine and metrics."""
 from .engine import Backtester
-from .metrics import calculate_metrics, format_metrics
+from .metrics import calculate_metrics, format_metrics, save_performance_table, save_trades_table
 
-__all__ = ["Backtester", "calculate_metrics", "format_metrics"]
+__all__ = ["Backtester", "calculate_metrics", "format_metrics", "save_performance_table", "save_trades_table"]
